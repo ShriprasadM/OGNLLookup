cb3ad6cb007c03a01581f588fcb896f4ad8f22b2
Shriprasad Marathe <shriprasad.marathe@pubmatic.com>
2024-06-25 10:31:28 +0530

    Working

--- ortb_lookup_example.go
+++ ortb_lookup_example.go
@@ -7,19 +7,23 @@ import (
 	"github.com/prebid/openrtb"
 )
 
-func generateBidResponseExample() {
+func generateBidResponseOGNLMap() {
 	// initF(openrtb.BidResponse{}, "bidResponse", "./bid_response_ognl.go")
 
-	bidResponse := openrtb.BidResponse{
-		BidID: "233e",
-		SeatBid: []openrtb.SeatBid{
-			{Bid: []openrtb.Bid{{AdM: "native"}}, Seat: "pubmatic"},
-			{Bid: []openrtb.Bid{{AdM: "video"}, {AdM: "banner"}}},
-		},
-	}
+}
+
+var bidResponse = openrtb.BidResponse{
+	BidID: "233e",
+	SeatBid: []openrtb.SeatBid{
+		{Bid: []openrtb.Bid{{AdM: "native"}}, Seat: "pubmatic"},
+		{Bid: []openrtb.Bid{{AdM: "video"}, {AdM: "<VAST></VAST>"}, {AdM: "<VAST1></VAST1>"}}},
+	},
+}
+
+func admLookup() {
 	bid, err := mognl.LookUpBidResponse("bidresponse.seatbid.bid.adm", bidResponse, mognl.IndexInfo{
 		SeatBidIndex: 1,
-		BidIndex:     1,
+		BidIndex:     44,
 	}, "")
 
 	if err == nil {
@@ -27,15 +31,13 @@ func generateBidResponseExample() {
 	} else {
 		fmt.Println(err)
 	}
+}
 
+func seatLookup() {
 	seat, err1 := mognl.LookUpBidResponse("bidresponse.seatbid.seat", bidResponse, mognl.IndexInfo{}, "")
 	if err1 == nil {
 		fmt.Println(seat)
 	} else {
 		fmt.Println(err1)
 	}
-}
-
-func generateBidReuestExample() {
-
 }
